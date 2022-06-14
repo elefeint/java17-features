fe086116c734c31c67b52bb720cce314124ff661
Elena Felder <41136058+elefeint@users.noreply.github.com>
2022-06-14 17:34:20 -0400

    rename from comment

--- src/test/java/com/examples/MultiLineLiteralsTest.java
+++ src/test/java/com/examples/MultiLineLiteralsTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Example from Spring Cloud GCP's DatastoreSampleApplicationIntegrationTests.
  */
-public class MultiLineCommentsTest {
+public class MultiLineLiteralsTest {
 
     // With Java 11: tedious concatenation to achieve
     //   1) readability
@@ -28,11 +28,11 @@ public class MultiLineCommentsTest {
     // But wait, now the lines are longer than 120 characters. Can this be fixed?
     // Yep; it's as easy as escaping the physical newline character
     String validation17_soft_breaks = """
-            Using Pageable parameter
-            Singer{singerId='singer1', firstName='John', lastName='Doe', \
-            albums=[], firstBand=null, bands=, personalInstruments=}
-            Singer{singerId='singer2', firstName='Jane', lastName='Doe', \
-            albums=[Album{albumName='a', date=2012-01-20}""";
+                Using Pageable parameter
+                Singer{singerId='singer1', firstName='John', lastName='Doe', \
+                albums=[], firstBand=null, bands=, personalInstruments=}
+                Singer{singerId='singer2', firstName='Jane', lastName='Doe', \
+                albums=[Album{albumName='a', date=2012-01-20}""";
 
     @Test
     public void theyAreAllTheSameString() {
